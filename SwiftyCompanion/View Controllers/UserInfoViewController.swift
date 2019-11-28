4b3171cee49705da1f4d8ddbc46969aae74240da
akulaiev <kulaeva.a@ukr.net>
2019-11-28 23:21:57 +0200

    added user info vc

---
+++ SwiftyCompanion/View Controllers/UserInfoViewController.swift
@@ -0,0 +1,20 @@
+//
+//  UserInfoViewController.swift
+//  SwiftyCompanion
+//
+//  Created by Anna Koulaeva on 28.11.2019.
+//  Copyright © 2019 Anna Koulaeva. All rights reserved.
+//
+
+import UIKit
+
+class UserInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func logoutButtonTapped(_ sender: UIBarButtonItem) {
+        
+    }
+}
---
+++ SwiftyCompanion/View/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  SwiftyCompanion
+//
+//  Created by Anna Koulaeva on 28.11.2019.
+//  Copyright © 2019 Anna Koulaeva. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var paramName: UILabel!
+    @IBOutlet weak var paramIcon: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
